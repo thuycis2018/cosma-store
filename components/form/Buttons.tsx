341e353c1140b4cd82d22c1bb224fffb0d5c4fa7
TL <thuycis@gmail.com>
2024-10-28 15:10:21 -0700

    fix(buttons): add back IconButton

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -4,6 +4,7 @@ import { ReloadIcon } from "@radix-ui/react-icons";
 import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { LuTrash2, LuPenSquare } from "react-icons/lu";
 
 type btnSize = "default" | "lg" | "sm";
 
@@ -38,3 +39,31 @@ export function SubmitButton({
     </Button>
   );
 }
+
+type actionType = "edit" | "delete";
+export const IconButton = ({ actionType }: { actionType: actionType }) => {
+  const { pending } = useFormStatus();
+
+  const renderIcon = () => {
+    switch (actionType) {
+      case "edit":
+        return <LuPenSquare />;
+      case "delete":
+        return <LuTrash2 />;
+      default:
+        const never: never = actionType;
+        throw new Error(`Invalid action type: ${never}`);
+    }
+  };
+
+  return (
+    <Button
+      type='submit'
+      size='icon'
+      variant='link'
+      className='p-2 cursor-pointer'
+    >
+      {pending ? <ReloadIcon className=' animate-spin' /> : renderIcon()}
+    </Button>
+  );
+};
