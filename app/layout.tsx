527c2ee42de0bd14bd610d5e33c98f2b0af111c5
TL <thuycis@gmail.com>
2024-11-01 12:01:27 -0700

    chore(analytics): temp change

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,22 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en' suppressHydrationWarning>
+        <head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', { send_page_view: false });
+              `,
+            }}
+          />
+        </head>
         <body className={inter.className}>
           <Providers>
             <Navbar />
